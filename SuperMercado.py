d5848342b181c5a6f01a3ae21fbba487f2329833
QueijoRalado <felipe.gdelima10@gmail.com>
2025-02-06 17:31:16 -0300

    Update SuperMercado.py

--- SuperMercado.py
+++ SuperMercado.py
@@ -3,10 +3,6 @@ carrinho = []
 valor_compras = 0
 n_produtos = 0
 
-#for produto, preco in congelados_açougue.items():
-        #    print(f"{produto} - R$ {preco}")
-        #ISSO AQUI EM CIMA PRINTA O VALOR E A CHAVE DOS DICIONÁRIOS, IMPORTANTE
-
 # ------------------------------------------------------------- SETOR ALIMENTÍCIO -------------------------------------------------------------
 congelados_açougue = {
     "Peito de Frango c/ Osso (KG)": 22.20,
@@ -121,9 +117,10 @@ precos_bolsa_acessorio = list(bolsa_acessorio.values())
 
 cozinha = {
     "Fogão Eletrolix 5 Bocas ": 1221.00,
-    "Microondas 110 Volts Eletrolix": 699.99,
+    "Microondas 110 Volts Electrolix": 699.99,
     "Geladeira Brastamp 3 Portas": 4299.00,
-    "Freezer Degalo 1 Porta": 2399.00
+    "Freezer Degalo 1 Porta": 2399.00,
+    "Liquidificador Mundial Turbo Power": 85.90
 }
 
 tudo_cozinha = list(cozinha.items())
@@ -131,18 +128,85 @@ produtos_cozinha = list(cozinha.keys())
 precos_cozinha  = list(cozinha.values())
 
 ventilação = {
-    "Ventilador de Mesa Britanha ": 179.90,
+    "Ventilador de Mesa Britanha": 179.90,
     "Climatizador Portátil Ventisal": 535.90,
     "Ar Condicionado Portátil FOS": 2199.00,
-    "Freezer Degalo 1 Porta": 2399.00
+    "Ar Condicionado Portátil PHALCO": 1599.00
 }
 
-tudo_cozinha = list(cozinha.items())
-produtos_cozinha = list(cozinha.keys())
-precos_cozinha  = list(cozinha.values())
+tudo_ventilação = list(ventilação.items())
+produtos_ventilação = list(ventilação.keys())
+precos_ventilação  = list(ventilação.values())
 
+tv_informática = {
+    "Smart TV LGG": 1779.00,
+    "Smart TV Samsunga": 3149.00,
+    "Ishame 15": 1399.00,
+    "Ishame 14": 1079.90,
+    "Notebook Samsunga das Galaxias": 2199.00,
+    "Notebook Samsunga das Galaxias 2": 2499.00
+}
 
-# ------------------------------------------------------------- SETOR DOS CUIDADOS PESSOAIS -------------------------------------------------------------
+tudo_tv_informática = list(tv_informática.items())
+produtos_tv_informática = list(tv_informática.keys())
+precos_tv_informática  = list(tv_informática.values())
+
+limpeza = {
+    "Máquina de Lavar Brastempo": 1999.00,
+    "Tanquinho Colormarq Semi-Automático": 3149.00,
+    "Lava Louças Electrolix": 2599.00,
+    "Aspirador de Pó WOP": 699.00,
+}
+
+tudo_limpeza = list(limpeza.items())
+produtos_limpeza = list(limpeza.keys())
+precos_limpeza  = list(limpeza.values())
+
+# ------------------------------------------------------------- SETOR DA HIGIENE -------------------------------------------------------------
+
+higiene_oral = {
+    "Escova Dental Elétrica Colgaty (Unidade)": 50.50,
+    "Escova Dental Colgaty (Unidade)": 6.75,
+    "Creme Dental Colgaty 180g": 9.69,
+    "Fio Dental Jo & Jo Menta 100 Metros": 13.59,
+    "Enxaguante Antisséptico Bucal Listarine 500ml": 14.99,
+}
+
+tudo_higiene_oral = list(higiene_oral.items())
+produtos_higiene_oral = list(higiene_oral.keys())
+precos_higiene_oral  = list(higiene_oral.values())
+
+cuidados_pessoais = {
+    "Papel Higiênico Folha Dupla Nove 12 Rolos (30 Metros Cada)": 21.09,
+    "Papel Higiênico Folha Tripla Nove 12 Rolos (30 Metros Cada)": 33.49,
+    "Haste Flexível Cotonetes Jo & Jo": 7.69,
+    "Fio Dental Jo & Jo Menta 100 Metros": 13.59,
+    "Creme Hidratante Corporal Lichia Organik Xhop 250ml": 24.20,
+    "Creme Hidratante Para o Rosto Natural 100ml": 13.99,
+    "Sabão em Barra Luz 85g": 2.29,
+    "Shampoo Nature 300ml": 9.90, 
+    "Sabão em Barra Luz Hidradante Antiacne 300ml": 10.39, 
+    "Shampoo Nature Antiquedas e Crescimento Lumina 300ml": 19.90, 
+}
+
+tudo_cuidados_pessoais = list(cuidados_pessoais.items())
+produtos_cuidados_pessoais = list(cuidados_pessoais.keys())
+precos_cuidados_pessoais  = list(cuidados_pessoais.values())
+
+perfumes = {
+    "Desodorante Antiodor Aerosol Dave 150ml": 18.89,
+    "Antitranspirante Aerosol Rexana Extra 150ml": 20.39,
+    "Desodorante Antitranspirante Ace Urban 152ml": 10.49,
+    "Desodorante Antitranspirante Roll-on Dave 50ml": 12.69,
+    "Desodorante Deep Blue Quazer Colônia 100ml": 81.90,
+    "Body Splash Obsessed Desodorante Colônia 200ml": 44.80
+}
+
+tudo_perfumes = list(perfumes.items())
+produtos_perfumes = list(perfumes.keys())
+precos_perfumes  = list(perfumes.values())
+
+# ------------------------------------------------------------------ SUPER MERCADO -------------------------------------------------------------
 
 class super_mercado():
     def __init__(self):
@@ -153,7 +217,7 @@ class super_mercado():
             print("( 1 ) - Alimentos")
             print("( 2 ) - Roupas")
             print("( 3 ) - Eletrodomésticos")
-            print("( 4 ) - Cuidados Pessoais\n")
+            print("( 4 ) - Higiene\n")
             print("( 5 ) - Finalizar a Compra")
             escolha = int(input("---------------------------------------------\nBem vindo(a) ao Super Mercado Nuvelu! Oque deseja comprar?\n---------------------------------------------\n"))
             if escolha <1 or escolha>5:
@@ -166,7 +230,7 @@ class super_mercado():
             elif escolha==3:
                 self.eletro()
             elif escolha==4:
-                self.cuidados()
+                self.higiene()
             elif escolha==5:
                 self.finalizar()
     def alimentos(self):
@@ -223,11 +287,54 @@ class super_mercado():
             elif escolha==6:
                 self.menu()
     def eletro(self):
-        print("receba")
-    def cuidados(self):
-        print("giggity giggity")
+        escolha = -1
+        print("-"*40)
+        while escolha <1 or escolha>5:
+            print("SETOR DS ELETRODOMÉSTICOS")
+            print("( 1 ) - Cozinha")
+            print("( 2 ) - Tv e Informática")
+            print("( 3 ) - Ventilação")
+            print("( 4 ) - Limpeza\n")
+            print("( 5 ) - Voltar ao Menu Principal")
+
+            escolha = int(input("----------------------------------------\nQue parte do setor alimentício deseja dar uma olhada?\n----------------------------------------\n"))
+            if escolha <1 or escolha>6:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*40)
+            elif escolha==1:
+                self.cozinha()
+            elif escolha==2:
+                self.tv_informática()
+            elif escolha==3:
+                self.ventilação()
+            elif escolha==4:
+                self.limpeza()
+            elif escolha==5:
+                self.menu()
+    def higiene(self):
+        escolha = -1
+        print("-"*40)
+        while escolha<1 or escolha>4:
+            print("SETOR DS ELETRODOMÉSTICOS")
+            print("( 1 ) - Higiêne Oral")
+            print("( 2 ) - Cuidados Pessoais")
+            print("( 3 ) - Perfumes\n")
+            print("( 4 ) - Voltar ao Menu Principal")
+
+            escolha = int(input("----------------------------------------\nQue parte do setor alimentício deseja dar uma olhada?\n----------------------------------------\n"))
+            if escolha <1 or escolha>6:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*40)
+            elif escolha==1:
+                self.higiene_oral()
+            elif escolha==2:
+                self.cuidados_pessoais()
+            elif escolha==3:
+                self.perfumes()
+            elif escolha==4:
+                self.menu()
     def finalizar(self):
-        print("que show da xuxa é esse hein kkkk nossa senhora se mate")
+        print("Compra Finalizada!")
 
     # --------------------------------------------------- ALTERNATIVAS DENTRO DOS SETORES ---------------------------------------------------
 
@@ -370,7 +477,7 @@ class super_mercado():
         escolha=-1
         print("-"*40)
         while escolha!=6:
-            print("SETOR DO ESTILO")
+            print("SETOR DAS CAMISETAS")
             n = 0
             for produto, preco in camisetas.items():
                 n+=1
@@ -402,7 +509,7 @@ class super_mercado():
         escolha=-1
         print("-"*40)
         while escolha!=6:
-            print("SETOR DO ESTILO")
+            print("SETOR DOS CALÇADOS")
             n = 0
             for produto, preco in sapatos.items():
                 n+=1
@@ -434,7 +541,7 @@ class super_mercado():
         escolha=-1
         print("-"*40)
         while escolha!=6:
-            print("SETOR DO ESTILO")
+            print("SETOR DAS CALÇAS")
             n = 0
             for produto, preco in calcas.items():
                 n+=1
@@ -466,7 +573,7 @@ class super_mercado():
         escolha=-1
         print("-"*40)
         while escolha!=6:
-            print("SETOR DO ESTILO")
+            print("SETOR DOS SHORTS")
             n = 0
             for produto, preco in shorts.items():
                 n+=1
@@ -498,7 +605,7 @@ class super_mercado():
         escolha=-1
         print("-"*40)
         while escolha!=6:
-            print("SETOR DO ESTILO")
+            print("SETOR DAS BOLSAS E ACESSÓRIOS")
             n = 0
             for produto, preco in bolsa_acessorio.items():
                 n+=1
@@ -525,7 +632,230 @@ class super_mercado():
 
     # ELETRODOMÉSTICOS: --------------------------------------------------------------------------------------------------------------------------
 
-    # CUIDADOS PESSOAIS: --------------------------------------------------------------------------------------------------------------------------
+    def cozinha(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=6:
+            print("SETOR DA COZINHA")
+            n = 0
+            for produto, preco in cozinha.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==6:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_cozinha[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_cozinha[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_cozinha[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
+
+    def ventilação(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=5:
+            print("SETOR DA VENTILAÇÃO")
+            n = 0
+            for produto, preco in ventilação.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==5:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_ventilação[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_ventilação[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_ventilação[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
+
+    def tv_informática(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=7:
+            print("SETOR DA TV E INFORMÁTICA")
+            n = 0
+            for produto, preco in tv_informática.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==7:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_tv_informática[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_tv_informática[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_tv_informática[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
+
+    def limpeza(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=5:
+            print("SETOR DA LIMPEZA")
+            n = 0
+            for produto, preco in limpeza.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==5:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_limpeza[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_limpeza[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_limpeza[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
 
 
+    # HIGIENE: --------------------------------------------------------------------------------------------------------------------------
+
+    def higiene_oral(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=6:
+            print("SETOR DA HIGIENE ORAL")
+            n = 0
+            for produto, preco in higiene_oral.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==6:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_higiene_oral[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_higiene_oral[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_higiene_oral[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
+
+    def cuidados_pessoais(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=6:
+            print("SETOR DOS CUIDADOS PESSOAIS")
+            n = 0
+            for produto, preco in cuidados_pessoais.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==6:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_cuidados_pessoais[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_cuidados_pessoais[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_cuidados_pessoais[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
+
+    def perfumes(self):
+        global valor_compras
+        global carrinho
+        global n_produtos
+        escolha=-1
+        print("-"*40)
+        while escolha!=6:
+            print("SETOR DA LIMPEZA")
+            n = 0
+            for produto, preco in perfumes.items():
+                n+=1
+                print(f"( {n} ) - {produto} - R$ {preco:.2f}")
+            print(f"\n( {n+1} ) - Voltar ao Setor Dos Eletrodomésticos")
+            escolha = int(input("\nEscolha o item desejado: \n"))
+            print("-"*40)
+            if escolha==6:
+                self.eletro()
+            elif escolha>6 or escolha<1:
+                print(f"'{escolha}' não é um valor válido, por favor, tente novamente (PS: Escolha conforme os números na esquerda!)")
+                print("-"*30)
+            else:
+                print (f"{produtos_perfumes[escolha-1]} adicionado ao carrinho!")
+                valor_compras = valor_compras + precos_perfumes[escolha-1]
+                print (f"Valor total do carrinho: {valor_compras:.2f}")
+                carrinho.append(tudo_perfumes[escolha-1])
+                print ("Produtos atuais no carrinho:")
+                n_produtos=0
+                for produto in carrinho:
+                    n_produtos+=1
+                    print(f"[ {n_produtos} ] {produto}")
+                print("-"*40)
 super_mercado()
