92fdc220a9d9d830e0349b4ad84c889c849edd80
Felipe Gonçalves de Lima <67613002+QueijoRalado@users.noreply.github.com>
2025-02-06 17:53:53 -0300

    Update Pneu.py

--- Pneu.py
+++ Pneu.py
@@ -6,6 +6,8 @@ chegar na pressão correta.
 Sua ajuda foi requisitada para desenvolver o programa da próxima bomba da SBC - Sistemas de Bombas Computadorizadas.
 
 Escreva um programa que, dada a pressão desejada digitada pelo motorista e a pressão do pneu lida pela bomba, indica a diferença entre a pressão desejada e a pressão lida.
+
+QueijoRalado falando, não tenho nada a falar
 ''''
 
 import random
